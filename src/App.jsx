631e6d38e1308f26a860bfeb5c73f373dc43b66d
William Mosquera <wjmmk@gmail.com>
2021-05-28 10:57:02 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,76 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import './App.css';
+
+function App(props) {
+  
+  const [notes, setNotes] = useState([]);
+  const [newNote, setNewNote] = useState(''); 
+  const [showAll, setShowAll] = useState(true);
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
+  }, [])
+
+  //console.log('render', notes.length, 'notes')
+ 
+  const addNote = (event) => {
+    event.preventDefault()
+    
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() > 0.5,
+      id: notes.length + 1,
+    }
+  
+    axios
+    .post('http://localhost:3001/notes', noteObject)
+    .then(response => {
+      //console.log(response)
+      setNotes(notes.concat(noteObject))
+      setNewNote('')
+    })
+  }
+  
+  const handleNoteChange = (event) => {
+    //console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
+
+  const notesToShow = showAll ? notes : notes.filter(note => note.important === true)
+    console.log(notesToShow)
+  return (
+    
+    <div>
+      <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all' }
+        </button>
+      </div>  
+      <ul>
+       {notesToShow.map( note => 
+          <li key={note.id}> 
+              {note.content}
+          </li>
+       )}
+      </ul>
+      <form onSubmit={addNote}>
+        <input 
+          value={newNote} 
+          onChange={handleNoteChange}
+        />
+        <button type="submit">save</button>
+      </form>   
+    </div>
+  )
+}
+
+export default App;
